d1f1ef905f62caa0027b29324ce2ae4ec88df494
hrishita-vaish <vaish.hrishita@tcs.com>
2013-01-23 21:34:16 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectBook::Application.config.secret_token = '5aa1584fa73541b3d50884136034f4d7a0afbf8c7b4b26731173a96f7b5acde9aeb5ab7d2751dc02797ae3a7eb5c7fe702c3bfea7a5fbd4560bfb273b1884c7f'
